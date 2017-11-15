395e108ed85611d1f51c548d8ee3670b2a4a4438
moonbe77 <moonbe77@gmail.com>
2017-11-15 17:33:28 -0300

    comenzando con el ejercicio de promesas

--- clase2/ejPromise.js
+++ clase2/ejPromise.js
@@ -19,7 +19,52 @@ function postDetail (postId) {
         });
 }
 
-postDetail(1)
-.then(function (respuesta1) {
-    console.log(respuesta1);    
-});
+/*postDetail(1)
+    .then(function (respuesta1) {
+        console.log(respuesta1);
+    });
+
+postDetail(2)
+    .then(function (respuesta2) {
+        console.log(respuesta2);
+    });*/
+
+let promDetail1 = postDetail(1);
+let promDetail2 = postDetail(2);
+
+/*Promise.all([promDetail1, promDetail2])
+    .then(function (details) {
+        let detail1 = details[0];
+        let detail2 = details[1];
+
+        console.log(detail1);
+        console.log(detail2);
+    });*/
+
+let promUsers = requestProm('https://jsonplaceholder.typicode.com/users/');
+let promPosts = requestProm('https://jsonplaceholder.typicode.com/posts/');
+
+Promise
+    .all([promUsers, promPosts])
+    .then(function (values) {
+        let users = JSON.parse(values[0]);
+        let posts = JSON.parse(values[1]);
+
+        let lista = post.map(function(){
+            let value = users.find(function (u) {
+                return (users.id == post.);
+            });
+        })
+       
+        // Tomar el listado de posts
+        // y por cada post reemplezar el userId por el user.
+        
+
+    })
+    .then(function (listaCompleta) {
+        console.log(listaCompleta);
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
+
